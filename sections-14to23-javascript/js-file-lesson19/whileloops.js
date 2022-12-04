5b65e8fe8c7445f086fa3a69775e0f1c82256683
gabrieljte <suppdevelopment@gmail.com>
2022-12-04 11:49:13 -0400

    🔁 loops + console todo list

---
+++ sections-14to23-javascript/js-file-lesson19/whileloops.js
@@ -0,0 +1,24 @@
+let promptwhile = prompt('while loops') 
+if (promptwhile === 'sim') {
+    // let start = 1
+    //     while (start < 50) {
+    //         start++
+    //         console.log(start) // melhor colocar while após
+    // }
+    // 
+    // let senha = 190507
+    // let login = prompt('digite sua senha')
+    // let maxattempts = 0
+    // while (login != senha) {
+    //     prompt('digite a senha novamente, ela estava errada')
+    //     maxattempts ++
+    //     if (maxattempts === 3) {
+    //         alert('você atingiu o limite máximo')
+    //         break; //break tem que ser usado após todas as declarações no if statement
+    //     }
+    // }
+
+} else {
+    console.log('sem while loops')
+}
+
